6fcb03e71fd9ba7776f159fd10b4d0c93167bd7b
Jacob Zimmerman <d00425031@dmail.dixie.edu>
2024-02-20 16:08:27 -0700

    first origional table created

---
+++ populate_database.bash
@@ -0,0 +1,10 @@
+./face create
+
+for i in {1..10}; do
+    ./face adduser $i"@gmail.com"
+done
+
+./face addinterest sports
+./face addinterest smoking
+./face addinterest yoga
+./face addinterest reading
