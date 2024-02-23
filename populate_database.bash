05b8fa6a0a9bf1eb800e0a91173546fb7e91875e
calvin.hunsaker <calvin.hunsaker@utahtech.edu>
2024-02-23 11:08:02 -0700

    added a get and a couple tables

--- populate_database.bash
+++ populate_database.bash
@@ -2,9 +2,13 @@
 
 for i in {1..10}; do
     ./face adduser $i"@gmail.com"
+    ./face addaccount $i"@gmail.com"  $i"user"
+    ./face addpost $i"of great posts"  $i
 done
 
 ./face addinterest sports
 ./face addinterest smoking
 ./face addinterest yoga
 ./face addinterest reading
+./face addcomment 2 3 trash
+./face getcomment
