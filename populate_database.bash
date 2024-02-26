199bcb4684c548663a36f3c875656edd0faac684
Jacob Zimmerman <d00425031@dmail.dixie.edu>
2024-02-26 16:54:15 -0700

    add account/post working

--- populate_database.bash
+++ populate_database.bash
@@ -2,7 +2,14 @@
 
 for i in {1..10}; do
     ./face adduser $i"@gmail.com"
+    ./face addaccount $i"@gmail.com" "user"$i 
+    ./face addpost $i
 done
+
+for i in {1..10}; do 
+    ./face addaccount $i"@gmail.com" "user"$i 
+done
+
 
 ./face addinterest sports
 ./face addinterest smoking
