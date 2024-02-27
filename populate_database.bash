2e0609024af1abef8bddccdc4e1d71b4d2f7a699
calvin.hunsaker <calvin.hunsaker@utahtech.edu>
2024-02-26 17:52:57 -0700

    rebase

--- populate_database.bash
+++ populate_database.bash
@@ -17,9 +17,16 @@ for i in {1..10}; do
     ./face addaccountinterest $i $(((($i + 6) % 11) + 1))
 done
 
+./face addinterest sports
+./face addinterest smoking
+./face addinterest yoga
+./face addinterest reading
+./face addinterest reading
+./face addinterest smoking
 ./face addcomment 2 3 trash
 #./face getcomment
 #./face getpost
 #./face getuser
 #./face getaccount
 #./face getinterest
+./face getfirstinterestingquery
